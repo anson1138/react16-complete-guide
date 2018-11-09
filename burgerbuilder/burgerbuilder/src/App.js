c034115a1231b63b4f5c543da05a593c79d4d45e
Anson Kee <anson.kee@jobvite-inc.com>
2018-11-08 20:11:52 -0800

    layout

--- burgerbuilder/burgerbuilder/src/App.js
+++ burgerbuilder/burgerbuilder/src/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import Layout from './components/Layout/Layout.js';
 
 class App extends Component {
   render() {
     return (
       <div>
+      <Layout>
+      <p>Hi test</p>
+      </Layout>
+
 
       </div>
     );
