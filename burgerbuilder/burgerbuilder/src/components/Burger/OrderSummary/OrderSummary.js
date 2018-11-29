72b7c69dbbca04f7043a392704ddfd4e1e73195e
Anson Kee <anson.kee@jobvite-inc.com>
2018-11-29 07:42:30 -0800

    restructure folders by moving Layout to hoc since Layout simply wraps other components

--- burgerbuilder/burgerbuilder/src/components/Burger/OrderSummary/OrderSummary.js
+++ burgerbuilder/burgerbuilder/src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,31 +1,38 @@
-import React from 'react';
-import Aux from '../../../hoc/Aux';
+import React, { Component } from 'react';
+import Aux from '../../../hoc/Aux/Aux';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients)
-    .map(igKey => {
+class OrderSummary extends Component {
+    // This class can be converted back to functional stateless component
+    // Made it to class so can add debug shouldComponentUpdate to see
+    // if props updating state uncessearily.
+    componentWillUpdate() {
+        console.log('[OrderSummary] WillUpdate')
+    }
+    render () {
+        const ingredientSummary = Object.keys(this.props.ingredients)
+        .map(igKey => {
+            return (
+                <li key={igKey}>
+                    <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {this.props.ingredients[igKey]}
+                </li>
+            );
+        });
+
         return (
-            <li key={igKey}>
-                <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
-            </li>
+            <Aux>
+                <h3>Your Order</h3>
+                <p>A delicious burger with the following ingredients:</p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p>Continue to Checkout?</p>
+                <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
+                <Button btnType="Danger" clicked={this.props.purchaseCancelled}>CANCEL</Button>
+                <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
+            </Aux>
         );
-    });
-
-    return (
-        <Aux>
-            <h3>Your Order</h3>
-            <p>A delicious burger with the following ingredients:</p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p>Continue to Checkout?</p>
-            <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
-            <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
-            <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
-        </Aux>
-    );
-
+    }
 };
 
-export default orderSummary;
+export default OrderSummary;
